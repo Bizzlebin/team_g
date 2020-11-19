2fb6229091ba275095e4f0c1f8e61f3d7451f8bc
Jeremiah Thomas <Bizzlebin@users.noreply.github.com>
2020-11-19 14:39:10 -0500

    = Readme Parser (fixed import statement)

--- readme_parser.py
+++ readme_parser.py
@@ -30,7 +30,7 @@
 from os import sys, path
 import re
 from datetime import date
-import resources.Queue.linkedqueue as linkedqueue
+import Queue.linkedqueue as linkedqueue
 # 
 # +++
 # Assignments
