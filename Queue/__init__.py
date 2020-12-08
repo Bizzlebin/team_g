415cecd6e6cf7dc522e47809e60ff8af795d4c58
Jeremiah Thomas <Bizzlebin@users.noreply.github.com>
2020-12-07 22:15:47 -0500

    + Init (blank init file for Queue)

---
+++ Queue/__init__.py
@@ -0,0 +1,17 @@
+# Init | Queue
+# 
+# https://github.com/Bizzlebin/team_g/blob/master/Queue/__init__.py
+# 
+# ***
+# 
+# By Jeremiah Thomas, et al
+# 
+# ***
+# 
+# Created 2020-12-07
+# 
+# +++
+# Description
+# 
+# A blank init file so Python recognizes the folder as a package for purposes of setuptools's """find_packages()""" function.
+# 
