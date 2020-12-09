a39a910607c2d5bed905df04ca75408e3d8b2620
ajimenez-wake <ajimenez2@waketeche.edu>
2020-12-09 00:12:53 -0500

    = fields.json (Copyright field removed. License field fixed to capture entire license section.) = test_usage_regex.py(Test inputs and answers tweaked to better represent the parsing of actual code.)

--- Tests/test_usage_regex.py
+++ Tests/test_usage_regex.py
@@ -12,28 +12,18 @@ def setup_module(module):
 '''
 Usage Section
 '''
-def test_copyright():
-	regex = re.compile(JSON_FILE['copyright']['regex'], re.M)
-	test_list = ["Copyright © Jeremiah Thomas, et al"
-"\n\nAll rights reserved. All copyright holders retain full copyright individually and may sublicense the work, up to and including a Ø (CC0) declaration."
-'\n\n"""THE WORK IS PROVIDED "AS IS" AND THE AUTHORS AND OWNERS DISCLAIM ALL WARRANTIES WITH REGARD TO THIS WORK INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHORS OR OWNERS BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS WORK."""']
-	matches = []
-	for test in test_list:
-		match = regex.match(test)
-		if match is not None:
-			matches.append(match.group(1))
-	test_answers = ["Jeremiah Thomas, et al"]
-	assert matches == test_answers, "USAGE COPYRIGHT - returns wrong string."
 
 def test_license():
 	regex = re.compile(JSON_FILE['license']['regex'], re.M)
-	test_list = ["Copyright © Jeremiah Thomas, et al"
-"\n\nAll rights reserved. All copyright holders retain full copyright individually and may sublicense the work, up to and including a Ø (CC0) declaration."
-'\n\n"""THE WORK IS PROVIDED "AS IS" AND THE AUTHORS AND OWNERS DISCLAIM ALL WARRANTIES WITH REGARD TO THIS WORK INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHORS OR OWNERS BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS WORK."""']
+	test_list = ['Copyright © Jeremiah Thomas, et al'
+				 '\n\nAll rights reserved. All copyright holders retain full copyright individually and may sublicense the work, up to and including a Ø (CC0) declaration.'
+				 '\n\n"""THE WORK IS PROVIDED "AS IS" AND THE AUTHORS AND OWNERS DISCLAIM ALL WARRANTIES WITH REGARD TO THIS WORK INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHORS OR OWNERS BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS WORK."""']
 	matches = []
 	for test in test_list:
 		match = regex.match(test)
 		if match is not None:
 			matches.append(match.group(1))
-	test_answers = ["All rights reserved. All copyright holders retain full copyright individually and may sublicense the work, up to and including a Ø (CC0) declaration."]
+	test_answers = ['Copyright © Jeremiah Thomas, et al'
+				 '\n\nAll rights reserved. All copyright holders retain full copyright individually and may sublicense the work, up to and including a Ø (CC0) declaration.'
+				 '\n\n"""THE WORK IS PROVIDED "AS IS" AND THE AUTHORS AND OWNERS DISCLAIM ALL WARRANTIES WITH REGARD TO THIS WORK INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHORS OR OWNERS BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS WORK."""']
 	assert matches == test_answers, "USAGE LICENSE - returns wrong string."
